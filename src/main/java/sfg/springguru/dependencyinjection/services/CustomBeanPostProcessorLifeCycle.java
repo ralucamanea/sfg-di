6385669152ea286d86ab4e9891701b921736d3a7
Raluca Manea <raluca96ok@gmail.com>
2021-05-26 21:01:52 +0200

    Section 3. Bean lifecycle

---
+++ src/main/java/sfg/springguru/dependencyinjection/services/CustomBeanPostProcessorLifeCycle.java
@@ -0,0 +1,19 @@
+package sfg.springguru.dependencyinjection.services;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomBeanPostProcessorLifeCycle implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        return bean;
+    }
+}
---
+++ src/main/java/sfg/springguru/dependencyinjection/services/LifeCycleDemoBean.java
@@ -0,0 +1,50 @@
+package sfg.springguru.dependencyinjection.services;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Component
+public class LifeCycleDemoBean implements InitializingBean, DisposableBean, BeanNameAware,
+        BeanFactoryAware, ApplicationContextAware {
+    //add messages and run in order to see a bean's life cycle
+    @Override
+    public void setBeanName(String s) {
+
+    }
+
+    @Override
+    public void destroy() throws Exception {
+
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+
+    }
+
+    @PostConstruct
+    public void postConstruct(){
+
+    }
+
+    @PreDestroy
+    public void preDestroy(){
+
+    }
+}
